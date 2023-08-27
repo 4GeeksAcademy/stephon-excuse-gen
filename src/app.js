1842e0f9ecc0fcb0738ddce790b6f2c8d92e503b
Stephon Griffen <56489291+stephon305@users.noreply.github.com>
2023-08-27 01:04:45 +0000

    finished.excuse.gen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
 };
+function generateExcuse() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
